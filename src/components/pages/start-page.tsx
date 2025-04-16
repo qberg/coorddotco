89c6326f509f1f92800c24af5b84258652431086
qberg <qbergnoe@gmail.com>
2025-04-16 09:38:05 +0530

    init fablab section

--- src/components/pages/start-page.tsx
+++ src/components/pages/start-page.tsx
@@ -1,6 +1,7 @@
 import CoordCommunity from '@/components/blocks/coord-communty'
 import CoordPhil from '@/components/blocks/coord-phil'
 import DiscoverArtisans from '@/components/blocks/discover-artisan'
+import FabLab from '@/components/blocks/fablab'
 import Library from '@/components/blocks/library'
 import Services from '@/components/blocks/services'
 import MainHero from '@/components/hero-blocks/main-hero'
@@ -12,6 +13,7 @@ const StartPage = () => {
       <CoordPhil />
       <Library />
       <Services />
+      <FabLab />
       <DiscoverArtisans />
       <CoordCommunity />
     </>

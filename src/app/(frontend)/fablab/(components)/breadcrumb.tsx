fc781eac5aef70e5003b8d34868ce4e2c44e5ad3
qberg <qbergnoe@gmail.com>
2025-04-26 10:10:48 +0530

    made it 85rem

---
+++ src/app/(frontend)/fablab/(components)/breadcrumb.tsx
@@ -0,0 +1,20 @@
+import { ChevronRight } from 'lucide-react'
+import Link from 'next/link'
+
+const BreadCrumb = () => {
+  return (
+    <div className="px-6 md:px-12 flex items-center justify-start gap-2">
+      <p className="font-light">
+        <Link href="/">Home</Link>
+      </p>
+
+      <ChevronRight className="text-accent-primary w-4 md:w-8" />
+
+      <p className="text-accent-primary font-bold">
+        <Link href="/fablab">Fablab</Link>
+      </p>
+    </div>
+  )
+}
+
+export default BreadCrumb
---
+++ src/app/(frontend)/fablab/page.tsx
@@ -0,0 +1,13 @@
+import BreadCrumb from '@/app/(frontend)/fablab/(components)/breadcrumb'
+import Header from '@/components/header'
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <BreadCrumb />
+    </>
+  )
+}
+
+export default Page
